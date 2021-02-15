83b3aaaf11d33383d5de521c9006d3e040e93a6e
Matt Wright <mttwright@gmail.com>
2021-02-15 22:20:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,43 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  post '/articles' do
+    @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+    redirect "/articles/#{@article.id}"
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = params[:article][:title]
+    @article.content = params[:article][:content]
+    @article.save
+    redirect "/articles/#{@article.id}"
+  end
+  
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect "/articles"
+  end
+  
 end
